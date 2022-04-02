9aca055e062ab129c318fa18e941055a1403e38a
Raphael Marchetti Calciolari <79259612+raphazik4@users.noreply.github.com>
2022-04-02 11:18:01 -0300

    Aula 02-04-2022: Microsservicos observacoes

--- microsservicos-lembretes-observacoes-26-03-2022/mss-lembretes/index.js
+++ microsservicos-lembretes-observacoes-26-03-2022/mss-lembretes/index.js
@@ -1,34 +1,42 @@
 const print = console.log
+print("Iniciando microsserviço de LEMBRETES.")
 const express = require('express')
 const app = express()
 
-// estamos aplicando um middleware - é executado antes de qualquer alvo definido abaixo (GET/POST)
+// Aplicação de middleware - é executado antes de qualquer alvo definido abaixo (GET/POST)
 app.use((req, res, next) => {
-    print("Requisição recebida...")
+    //print("Executando middleware...")
     next()
 })
-app.use(express.json())
+app.use(express.json()) // Próximo middleware, chamado no next() acima
+
+// Definição de dados
+const path = '/lembretes'
+const port = 4000
 
 const lembretes = {'teste': 'dados'} // objeto apontado pela variável não pode mudar, mas o objeto em si pode ser alterado
+
+// Variáveis auxiliares
 let contador = 0
 
-// Configuração do que fazer quando receber requisição do tipo GET
-// exemplo.com.br/lembretes
-app.get('/lembretes', (requestData, response) => {
+// GET - Configuração do que fazer quando receber requisição do tipo GET, exemplo.com.br/lembretes
+app.get(path, (requestData, response) => {
+    print("Requisição GET recebida.")
     response.send(lembretes)
 })
 
-// Configuração do que fazer quando receber requisição do tipo POST
-// exemplo.com.br/lembretes
-app.post('/lembretes', (requestData, response) => {
+// POST - Configuração do que fazer quando receber requisição do tipo POST, exemplo.com.br/lembretes
+app.post(path, (requestData, response) => {
+    print("Requisição POST recebida.")
     contador++
     //{texto: "Lembrete, como fazer café, ir dormir, etc."}
-    const { texto } = requestData.body
+    //const texto = requestData.body.texto - modo de pegar dados pelo js
+    const { texto } = requestData.body // desestruturação
     lembretes[contador] = {contador, texto} // json implícito: {contador: contador, texto: texto}
     response.status(201).send(lembretes[contador]) // status para criado - devolve
 })
 
-//
-app.listen(4000, () => {
+// Abrindo porta 4000 para receber requisições
+app.listen(port, () => {
     print("Lembretes. Escutando a porta 4000")
 })
--- microsservicos-lembretes-observacoes-26-03-2022/mss-observacoes/index.js
+++ microsservicos-lembretes-observacoes-26-03-2022/mss-observacoes/index.js
@@ -1,23 +1,45 @@
 const print = console.log
+print("Iniciando microsserviço de OBSERVAÇÕES.")
+const { json } = require('express')
 const express = require('express')
+const {v4: uuidv4, stringify} = require('uuid') // importa o modulo uuid e pega a referência da função uuidv4() da chave v4
 const app = express()
 
 // Middleware (executado) antes de ir pros destinos
 app.use(express.json())
 
+// Definição de dados
+const path = '/lembretes/:id/observacoes' // significa que o valor será substituido
+const port = 5000
+
+const observacoesPorLembreteId = {'': ''}
 const data = {'teste': 'dados'}
 
-//POST
-//host:porta/lembretes/id-lembrete/observacoes, ex host:4000/lembretes/2/observacoes
-app.post('/lembretes/:id/observacoes', (req, res) => { // o :id identifica que entre as 2 barras (anterior e posterior) haverá um valor
+// GET
+app.get(path, (req, res) => {
+    print("Requisição GET recebida.")
+    const requestId = req.params.id
+    if (observacoesPorLembreteId[requestId] === undefined) res.status(404).send("<html><body><h1 style='color:red'>ERRO 404</h1><p><img src = 'https://pbs.twimg.com/media/E5bbxjGWUAEwBnT.jpg'></p></body></html>")
+    else res.status(200).send(observacoesPorLembreteId[requestId])
     
 })
 
-//GET
-app.get('/lembretes/:id/observacoes', (req, res) => {
-    res.send(data)
+// POST - host:porta/lembretes/id-lembrete/observacoes, ex host:4000/lembretes/2/observacoes
+app.post(path, (req, res) => { // o :id identifica que entre as 2 barras (anterior e posterior) haverá um valor
+    print("Requisição POST recebida.")
+    const idObs = uuidv4()
+    const { texto } = req.body
+
+    // Atualizando dados
+    const requestId = req.params.id // pega-se os parâmetros da requisição - id
+    const observacoesDoLembrete = observacoesPorLembreteId[requestId] || [] // || [], verifica se a primeira parte da expressão é válida
+    observacoesDoLembrete.push({id: idObs, texto})
+    observacoesPorLembreteId[requestId] = observacoesDoLembrete
+
+    res.status(201).send("Recurso adicionado:\n" + JSON.stringify(observacoesDoLembrete))
 })
 
-app.listen(5000, () => {
+// Abrindo porta 5000 para receber requisições
+app.listen(port, () => {
     print('Lembretes. Porta 5000.')
 })
