f08ba55da8b34f2a03b348acdb33d62cf82d1b4b
Raphael Marchetti Calciolari <79259612+raphazik4@users.noreply.github.com>
2022-04-23 11:26:54 -0300

    Css e fontes

---
+++ css-23-04-2022/introducao-parte-3/cursos/ip-finder.js
@@ -0,0 +1,18 @@
+const axios = require('axios')
+
+const iplist = []
+let listindex = 0
+let falseIps = 0
+
+for(let i = 0; i < 256; i++){
+    axios.get(`http://10.33.109.${i}:5500/`).then((response) => {
+        if (response.status === 200){
+            iplist[listindex] = i
+            listindex++
+            console.log(iplist)
+        }
+    }).catch((error) => {
+        falseIps++
+    })
+}
+
