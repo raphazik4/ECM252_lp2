1d8206ee5828d6fab12d75305a7693d13eb58b7d
Raphael Marchetti Calciolari <79259612+raphazik4@users.noreply.github.com>
2022-04-30 11:10:18 -0300

    Primeira parte da aula angular

---
+++ projetos-angular/primeiro-app/src/app/ola-angular-cli/ola-angular-cli.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ola-angular-cli',
+  templateUrl: './ola-angular-cli.component.html',
+  styleUrls: ['./ola-angular-cli.component.css']
+})
+export class OlaAngularCliComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ projetos-angular/primeiro-app/src/app/ola-angular/ola-angular.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core'
+
+// seletor - nome da tag que vai instanciar o componente no html
+@Component({
+    selector: 'ola-angular', // pode ser qualquer nome
+    templateUrl: './ola-angular.component.html',
+    styleUrls: [
+        './ola-angular.component.css'
+    ]
+})
+export class OlaAngularComponent{
+
+}
