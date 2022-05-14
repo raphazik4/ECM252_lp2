bf4f129b75dcb7558987c577bb914da8af9cfe95
raphazik4 <rapha.marchetti@gmail.com>
2022-05-14 10:41:38 -0300

    Fim secao 2.7
    Renderização condigional da lista de clientes e feedback visual para quando ela está vazia

--- projetos-angular/cadastro-clientes/src/app/clientes/cliente-lista/cliente-lista.component.ts
+++ projetos-angular/cadastro-clientes/src/app/clientes/cliente-lista/cliente-lista.component.ts
@@ -8,16 +8,16 @@ import { Component, OnInit } from '@angular/core';
 export class ClienteListaComponent implements OnInit {
 
   clientes = [
-    {
-      nome: 'José',
-      fone: '123455678',
-      email: 'jose@email.com'
-    },
-    {
-      nome: 'Maria',
-      fone: '98765432',
-      enmail: 'maria@email.com'
-    }
+    // {
+    //   nome: 'José',
+    //   fone: '123455678',
+    //   email: 'jose@email.com'
+    // },
+    // {
+    //   nome: 'Maria',
+    //   fone: '98765432',
+    //   enmail: 'maria@email.com'
+    // }
   ]
 
   constructor() { }
