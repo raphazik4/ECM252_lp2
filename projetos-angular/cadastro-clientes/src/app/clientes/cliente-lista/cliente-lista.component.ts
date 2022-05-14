99f192de66e7932c036fe00957d149f1c26bc4ad
raphazik4 <rapha.marchetti@gmail.com>
2022-05-14 12:59:34 -0300

    Adicionado limpeza de subscribers para o observer pattern

--- projetos-angular/cadastro-clientes/src/app/clientes/cliente-lista/cliente-lista.component.ts
+++ projetos-angular/cadastro-clientes/src/app/clientes/cliente-lista/cliente-lista.component.ts
@@ -1,23 +1,29 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { Cliente } from '../cliente.model';
 import { ClienteService } from '../cliente.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-cliente-lista',
   templateUrl: './cliente-lista.component.html',
   styleUrls: ['./cliente-lista.component.css']
 })
-export class ClienteListaComponent implements OnInit {
+export class ClienteListaComponent implements OnInit, OnDestroy {
 
   clientes: Cliente[] = []
+  private clientesSubscription: Subscription
 
   constructor(private clienteService: ClienteService) { }
 
   ngOnInit(): void {
     this.clientes = this.clienteService.getClientes()
-    this.clienteService.getListaDeClientesAtualizada().subscribe((clientes: Cliente[]) => {
+    this.clientesSubscription = this.clienteService.getListaDeClientesAtualizada().subscribe((clientes: Cliente[]) => {
       this.clientes = clientes
     })
+  }
+
+  ngOnDestroy(): void {
+    this.clientesSubscription.unsubscribe()
   }
 
 }
