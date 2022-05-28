2534ae3d47091e9ff614d90c3fdc544bde3e0af6
raphazik4 <rapha.marchetti@gmail.com>
2022-05-28 12:57:37 -0300

    Instalamos o mongoose

---
+++ projetos-angular/mean-bonato/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { Cliente } from './clientes/cliente.model';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  clientes: Cliente[] = [];
+  onClienteAdicionado(cliente){
+    this.clientes = [...this.clientes, cliente];
+  }
+}
---
+++ projetos-angular/mean-bonato/src/app/clientes/cliente.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from "@angular/core";
+import { Cliente } from "./cliente.model";
+import { Subject } from "rxjs";
+import { HttpClient } from "@angular/common/http";
+
+@Injectable({ providedIn: 'root' })
+export class ClienteService {
+  private clientes: Cliente[] = [];
+  private listaClientesAtualizada = new Subject<Cliente[]>();
+
+  constructor(private httpClient: HttpClient){}
+
+  getClientes(): void {
+    this.httpClient.get<{mensagem: string,
+      clientes: Cliente[]}>('http://localhost:3000/api/clientes').subscribe(
+        (dados) => {
+          this.clientes = dados.clientes;
+          this.listaClientesAtualizada.next([...this.clientes]);
+        }
+      )
+  }
+
+  adicionarCliente(nome: string, fone: string, email:string){
+    const cliente: Cliente = {
+      nome: nome,
+      fone: fone,
+      email: email,
+    };
+    this.httpClient.post<{mensagem: string}>('http://localhost:3000/api/clientes',
+    cliente).subscribe(
+      (dados) => {
+        console.log(dados.mensagem);
+        this.clientes.push(cliente);
+        this.listaClientesAtualizada.next([...this.clientes]);
+      }
+    )
+  }
+
+  getListaDeClientesAtualizadaObservable(){
+    return this.listaClientesAtualizada.asObservable();
+  }
+}
